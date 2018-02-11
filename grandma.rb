678c7678808dc55553947739a9f58a673c408cb4
Joseph DiPeppe <jkdipeppe@gmail.com>
2018-02-11 03:51:25 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,13 +15,11 @@
 
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
-    return 
+    return
   elsif (string == 'WHAT DID YOU EAT TODAY?')
     return "NO, NOT SINCE 1938!"
   elsif string == 'WHAT?'
     return "NO, NOT SINCE 1938!"
-  elsif (string == 'I LOVE YOU PUMPKIN!')
-    return 'I LOVE YOU TOO PUMPKIN!'
   elsif string != string.upcase
     return 'HUH?! SPEAK UP, SONNY!'
   end

0c35e25aaad14db7c6d56f06578bde382b10808c
Joseph DiPeppe <jkdipeppe@gmail.com>
2018-02-11 03:47:44 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -20,6 +20,6 @@ def speak_to_grandma(string)
     puts "NO, NOT SINCE 1938!"
   elsif (string == 'I LOVE YOU GRANDMA!')
     puts 'I LOVE YOU TOO PUMPKIN!'
-  else
+  elsif string != string.upcase
     puts 'HUH?! SPEAK UP, SONNY!'
 end
